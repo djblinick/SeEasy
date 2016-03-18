bad06074aeb0baf7b91563b81b88d3cb848429ec
Zvi <zvikush90@gmail.com>
2016-03-18 05:37:24 -0700

    db manager and resources
    script to auto add categories and websites need revisioning

---
+++ server/env/rec_API/manage_db.py
@@ -0,0 +1,65 @@
+from rec_app.models import *
+import numpy as np
+import pandas as p
+#url	urlid	boilerplate	alchemy_category	alchemy_category_score	avglinksize	commonlinkratio_1	commonlinkratio_2	commonlinkratio_3	commonlinkratio_4	compression_ratio	embed_ratio	framebased	frameTagRatio	hasDomainLink	html_ratio	image_ratio	is_news	lengthyLinkDomain	linkwordscore	news_front_page	non_markup_alphanum_characters	numberOfLinks	numwords_in_url	parametrizedLinkRatio	spelling_errors_ratio
+DATA_PATH = './rec_app/resources/websites.tsv'
+
+CATEGORIES_PATH = "./rec_app/resources/clean_categories.txt"
+
+def load_data(self,path):
+    lines=[]
+    with open(path, 'r') as data:
+        for line in data:
+            lines.append(line.lower())
+    return lines
+
+
+class CatMan():
+    def create_entry(self,name):
+        c = Category(name=name)
+        c.save()
+
+    def create(self):
+        lines = load_data(CATEGORIES_PATH)
+        for line in lines:
+            self.create_entry(line)
+
+        print Website.objects.all()
+
+    def delete_all(self):
+        Category.objects.all().delete()
+
+class TSV():
+    def create_web(self,url, body):
+        w = Website(url=url,body=body)
+        w.save()
+        return w
+
+    def create_category(self, name):
+        if name:
+            c = Category(name=name)
+            c.save()
+            return c
+        return False
+
+    def create(self):
+        print "Loading text"
+
+        lines = list(np.array(p.read_table(DATA_PATH)))
+        for line in lines:
+            c=self.create_category(line[0])
+            w=self.create_web(line[1],line[2])
+            if c:
+                c.websites.add(w)
+                w.categories.add(c)
+                c.save()
+                w.save()
+
+        print Website.objects.all()[0]
+        print Category.objects.all()[0]
+
+    def delete_all(self):
+        Website.objects.all().delete()
+        Category.objects.all().delete()
+
+
--- server/env/rec_API/rec_app/models.py
+++ server/env/rec_API/rec_app/models.py
@@ -6,29 +6,29 @@ STANDARD_LENGTH = 20
 class Website(models.Model):
     url = models.URLField()
     body = models.TextField()
-    average_view_time = models.IntegerField(default=0)
-    categories = models.ManyToManyField('Category')
-    users_visited = models.ManyToManyField('Profile')
+    average_view_time = models.IntegerField(default=0, blank=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    users_visited = models.ManyToManyField('Profile', blank=True)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=STANDARD_LENGTH)
-    websites = models.ManyToManyField('Website')
+    websites = models.ManyToManyField('Website', blank=True)
 
 
 class Profile(models.Model):
     username = models.CharField(max_length=STANDARD_LENGTH)
     email = models.CharField(max_length=STANDARD_LENGTH)
-    websites = models.ManyToManyField('Website')
-    most_viewed_site = models.ForeignKey('Website',related_name='Profile.most_viewed_site+' ,on_delete=models.CASCADE)
+    websites = models.ManyToManyField('Website', blank=True)
+    most_viewed_site = models.ForeignKey('Website',related_name='Profile.most_viewed_site+' ,on_delete=models.CASCADE, blank=True)
 
 
 class Rec(models.Model):
-    answered = models.BooleanField()
-    url = models.ForeignKey('Website', related_name='Rec.url+' ,on_delete=models.CASCADE)
-    rec1 = models.ForeignKey('Website', related_name='Rec.rec1+' ,on_delete=models.CASCADE)
-    rec2 = models.ForeignKey('Website', related_name='Rec.rec2+' ,on_delete=models.CASCADE)
-    rec3 = models.ForeignKey('Website', related_name='Rec.rec3+' ,on_delete=models.CASCADE)
+    answered = models.BooleanField(default=False)
+    url = models.ForeignKey('Website', related_name='Rec.url+' ,on_delete=models.CASCADE, blank=True)
+    rec1 = models.ForeignKey('Website', related_name='Rec.rec1+' ,on_delete=models.CASCADE, blank=True)
+    rec2 = models.ForeignKey('Website', related_name='Rec.rec2+' ,on_delete=models.CASCADE, blank=True)
+    rec3 = models.ForeignKey('Website', related_name='Rec.rec3+' ,on_delete=models.CASCADE, blank=True)
     added = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
