8282415985d4c0512c19fe4c7371b1d9b8844492
Zvi <zvikush90@gmail.com>
2016-03-20 06:44:10 -0700

    same category url and clean body
    urls with same categories added to db well
    body of url added once cleaned from stop words and punctuation

--- server/env/rec_API/manage_db.py
+++ server/env/rec_API/manage_db.py
@@ -1,6 +1,10 @@
 from rec_app.models import *
 import numpy as np
 import pandas as p
+import re
+import string
+from django.core.exceptions import ObjectDoesNotExist
+from nltk.corpus import stopwords
 #url	urlid	boilerplate	alchemy_category	alchemy_category_score	avglinksize	commonlinkratio_1	commonlinkratio_2	commonlinkratio_3	commonlinkratio_4	compression_ratio	embed_ratio	framebased	frameTagRatio	hasDomainLink	html_ratio	image_ratio	is_news	lengthyLinkDomain	linkwordscore	news_front_page	non_markup_alphanum_characters	numberOfLinks	numwords_in_url	parametrizedLinkRatio	spelling_errors_ratio
 DATA_PATH = './rec_app/resources/websites.tsv'
 
@@ -13,6 +17,20 @@ def load_data(self,path):
             lines.append(line.lower())
     return lines
 
+def clean_body(body):
+    body = body.replace('{','')
+    body = body.replace('}','')
+    body = body.replace('"','')
+    body = body.replace('title:','')
+    body = body.replace('body:','')
+    body = body.replace('url:','')
+    body = body.replace('html','')
+    exclude = set(string.punctuation)
+    body = ''.join(ch for ch in body if ch not in exclude)
+    body = re.sub('^[0-9]+', '', body)
+    stop = stopwords.words('english')
+    body =' '.join([i for i in body.split() if i not in stop])
+    return body
 
 # class CatMan():
 #     def create_entry(self,name):
@@ -29,21 +47,33 @@ def load_data(self,path):
 #     def delete_all(self):
 #         Category.objects.all().delete()
 
+
+
 class TSV():
     def create_web(self,url, body):
-        w = Website(url=url,body=body)
-        w.save()
-        return w
+        try:
+            w = Website.objects.get(url=url)
+            return w
+        except ObjectDoesNotExist:
+            w = Website(url=url,body=clean_body(body))
+            w.save()
+            return w
+
+
 
     def create_category(self, name):
         if name:
-            c = Category(name=name)
-            c.save()
-            return c
+            try:
+                c = Category.objects.get(name=name)
+                return c
+            except ObjectDoesNotExist:
+                c = Category(name=name)
+                c.save()
+                return c
         return False
 
     def create(self):
-        print "Loading text"
+        print "Loading data"
 
         lines = list(np.array(p.read_table(DATA_PATH)))
         for line in lines:
@@ -55,8 +85,7 @@ class TSV():
                 c.save()
                 w.save()
 
-        print Website.objects.all()[0]
-        print Category.objects.all()[0]
+        print "Created from data"
 
     def delete_all(self):
         Website.objects.all().delete()
