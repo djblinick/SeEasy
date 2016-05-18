5f4324fa5676e132948fd0782dfddb39c40ccf7d
Zviksuh90 <zvikush90@gmail.com>
2016-05-18 16:04:31 +0300

    data base manager updates entries without category with dummy category in database

--- server/rec/db_manager.py
+++ server/rec/db_manager.py
@@ -1,10 +1,29 @@
-from get_url.models import *
+#!/usr/bin/python
 
-def all_web():
-    return Website.objects.all()
+import sqlite3
 
-def create_web_entry(url,body,category):
-    web = Website(url,body,category)
-    web.save()
+def get_category(url):
+    return "BLA"
 
-def top_in_category(count):
+
+conn = sqlite3.connect('db.sqlite3')
+print "Opened database successfully";
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+
+cursor = conn.execute("SELECT * FROM get_url_website WHERE category=\'\'")
+#'body', 'view_count', 'average_view_time', 'category', 'url'
+names = list(map(lambda x: x[0], cursor.description))
+print names
+
+for row in cursor:
+    print row
+    print row[4]
+    update_command = "UPDATE get_url_website SET (category) VALUES ("+get_category(row[4])+") WHERE url="+str(row[4])
+    conn.execute("""UPDATE get_url_website SET category = ? WHERE url= ? """, (get_category(row[4]),row[4]))
+
+
+conn.commit()
+print "Records created successfully";
+conn.close()
