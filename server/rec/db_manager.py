c0547133d6875ab26243214141f5f37d3f89ec84
Zviksuh90 <zvikush90@gmail.com>
2016-05-27 11:38:47 +0300

    parsed xml response finished update non categorized

--- server/rec/db_manager.py
+++ server/rec/db_manager.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python
+
 import requests
 import sqlite3
 import xml.etree.ElementTree as ET
 
+
 def get_category(url):
-    response1 = requests.post(
-        'http://uclassify.com/browse/uClassify/Topics/ClassifyUrl?readkey=PjwkZVJnxWHi&url=' + 'www.url.com')
-    print response1.content
-    root = ET.fromstring(response1.content)
-    print root
-    return response1.content
+    response = requests.post(
+        'http://uclassify.com/browse/uClassify/Topics/ClassifyUrl?readkey=PjwkZVJnxWHi&url=' + str(url))
+    print response.content
+    root = ET.fromstring(response.content)
+    class_names = []
+    for level1 in root:
+        atrr = level1.attrib
+        tag = level1.tag
+        if(level1.tag == "{http://api.uclassify.com/1/ResponseSchema}readCalls"):
+            for level2 in level1:
+                if(level2.tag == "{http://api.uclassify.com/1/ResponseSchema}classify"):
+                    for level3 in level2:
+                        for level4 in level3:
+                            print level4.attrib
+                            class_names.append(level4.attrib)
+    print class_names
+    newlist = sorted(class_names, key=lambda k: k['p'],reverse=True)
+    print newlist
+    print newlist[0]['className']
+    return newlist[0]['className']
 
 
 conn = sqlite3.connect('db.sqlite3')
