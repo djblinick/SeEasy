bc366f64bd59f4b0b80db72dd2a4c16b1475cbfe
Zviksuh90 <zvikush90@gmail.com>
2016-05-27 12:12:17 +0300

    update view to support update view count

--- server/rec/get_url/views.py
+++ server/rec/get_url/views.py
@@ -12,12 +12,14 @@ import random
 
 logger = logging.getLogger('django')
 
+
 # Create your views here.
 def index(request):
-	logger.debug('index')
-	return HttpResponse("SeEasy Django Index")
+    logger.debug('index')
+    return HttpResponse("SeEasy Django Index")
 
-def api(request,url):
+
+def api(request, url):
     logger.debug('***** url: ' + str(url))
     method = 'blank'
     json_data = 'null'
@@ -28,16 +30,6 @@ def api(request,url):
         json_data = get_recommented_urls(url)
     return HttpResponse(json_data)
 
-def similar(request, category):
-    top = Website.objects.filter(category=category).order_by('-view_count')
-    urls = []
-    #vcount = []
-    for web in top:
-        urls.append(web.url)
-        #vcount.append(web.view_count)
-
-    return HttpResponse(str(urls[0:3]))
-
 
 def get_recommented_urls(url):
     json_data = 'null'
@@ -45,22 +37,22 @@ def get_recommented_urls(url):
         url_entry = Website.objects.get(pk=url)
         logger.debug('******* category: ' + str(url_entry.category) + '*******')
         top3_urls_in_category_list = Website.objects.filter(category=url_entry.category).order_by('-view_count')[0:3]
-        json_data = serializers.serialize("json", top3_urls_in_category_list, fields = ('url','category'))
+        json_data = serializers.serialize("json", top3_urls_in_category_list, fields=('url', 'category'))
         logger.debug('*******  top 3 category: ' + str(json_data) + '*********')
     except Website.DoesNotExist:
         logger.debug('******** url does not exist in database ********')
-        category = get_category()
-        w = Website(url = url, view_count = 1, category=category)
-        logger.debug('********* website: ' + str(w)+ 'added to database')
-        w.save()
     return json_data
+
 
 def post_to_database(url):
     logger.debug('post')
+    url_entry = Website.objects.get(pk=url)
+    if url_entry == None:
+        logger.debug('********* existing website: ' + str(url_entry) + 'increment view count')
+        url_entry.view_count = url_entry.view_count + 1
+        url_entry.save()
+    else:
+        w = Website(url=url, view_count=1)
+        logger.debug('********* new website: ' + str(w) + 'added to database')
+        w.save()
 
-#TODO add function that returns the proper category
-def get_category():
-	sites = ['news', 'social', 'cs']
-	return (random.choice(sites))
-
-	 
